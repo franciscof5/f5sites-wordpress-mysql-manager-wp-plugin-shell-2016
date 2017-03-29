56ec674e62a956475872207a5b27a61a4068ec62
Francisco Mat <francisco@f5sites.com>
2017-03-29 09:51:26 -0300

    bug in TABLE_PREFIX

--- mysql-prompts/get-table-prefix.sh
+++ mysql-prompts/get-table-prefix.sh
@@ -1,14 +1,14 @@
 echo "Enter table prefix WITHOUT underline_, leave empty for: "$DATABASENAME"_"
-read TABLE_PREFIX_ENTERED
+read TABLE_PREFIX
 
-if  [[ -z "$TABLE_PREFIX_ENTERED" ]]; then
+if  [[ -z "$TABLE_PREFIX" ]]; then
 	#echo "Using default table prefix: $DEFAULT_TABLE_PREFIX"
 	#TABLE_PREFIX="$DEFAULT_TABLE_PREFIX"
 	echo "Using default table prefix: $DATABASENAME_"
 	TABLE_PREFIX="$DATABASENAME"_
 else
-	echo "Table prefix is: ${TABLE_PREFIX_ENTERED}_"
-	TABLE_PREFIX="${TABLE_PREFIX_ENTERED}_"
+	echo "Table prefix is: ${TABLE_PREFIX}_"
+	TABLE_PREFIX="${TABLE_PREFIX}_"
 fi
 
 
