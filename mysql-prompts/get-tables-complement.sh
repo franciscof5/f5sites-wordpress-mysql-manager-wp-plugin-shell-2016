eefef8a51ce1ed8f45776fd4c20ef42dc927a650
Francisco Mat <francisco@f5sites.com>
2017-05-12 15:37:23 -0300

    code structure improv

--- mysql-prompts/get-tables-complement.sh
+++ mysql-prompts/get-tables-complement.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Tip what tables inside database you want to use, backup dir ($LOCAL_TEMP_DIR)"
-echo "1 - Database: $DATABASENAME (all tables, ignoring table prefix previous entered)"
-echo "2 - Only tables with prefix $TABLE_PREFIX inside database $DATABASENAME"
+echo "1 - All Database: $DATABASENAME (all tables, ignoring table prefix previous entered)"
+echo "2 - Prefixed: only tables with prefix $TABLE_PREFIX inside database $DATABASENAME"
 echo "3 - WordPress posts tables (${TABLE_PREFIX}posts, ${TABLE_PREFIX}postmeta, ${TABLE_PREFIX}termmeta, ${TABLE_PREFIX}terms, ${TABLE_PREFIX}term_relationships, ${TABLE_PREFIX}term_taxonomy) (Best choice for auto-sync)"
 echo "4 - Posts - ${TABLE_PREFIX}posts"
 echo "5 - Options - ${TABLE_PREFIX}options"
