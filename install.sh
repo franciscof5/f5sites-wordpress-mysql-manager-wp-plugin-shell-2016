e8b975292eb4fe570272aefa8cfe9ce1274662f4
Francisco Mat <francisco@f5sites.com>
2017-07-16 03:24:31 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #/bin/bash
-echo "Installing wpsql-cli on bin directory to make avaiable globally, at any directory at the system"
+echo "Installing wpsql-cli on current user's bin directory to make avaiable globally, at any directory at the system"
 rm -rf ~/bin/wpsql-cli-folder
 rm -rf ~/bin/wpsql-cli
 mkdir ~/bin/wpsql-cli-folder
 cp -r * ~/bin/wpsql-cli-folder/
 ln -s ~/bin/wpsql-cli-folder/wpsql-cli.sh ~/bin/wpsql-cli
+echo "Installation successfully"
+echo "You can run wpsql-cli from any directory for the current user"
