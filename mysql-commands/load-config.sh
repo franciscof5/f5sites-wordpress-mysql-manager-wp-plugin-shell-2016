62e3458bea197a36bf1f7ba306f2225d537014b5
Francisco Mat <francisco@f5sites.com>
2020-03-15 15:22:10 -0300

    check conf file

--- mysql-commands/load-config.sh
+++ mysql-commands/load-config.sh
@@ -1,3 +1,10 @@
 # Configuration file
 echo "Load configuration file... (to change it open config.sh)"
-source config.sh
+file="config.sh"
+if [ -f "$file" ]
+then
+	echo "$file found."
+	source config.sh
+else
+	echo "Create a config.sh file, not foud"
+fi
