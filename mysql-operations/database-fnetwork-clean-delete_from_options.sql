98b08a5d43991d884b1dbd9d38329a798059015b
Francisco Mat <francisco@f5sites.com>
2021-08-15 11:46:55 -0300

    add clean routine

---
+++ mysql-operations/database-fnetwork-clean-delete_from_options.sql
@@ -0,0 +1,105 @@
+-- themes_mod nao pode
+-- rs_ pega sidebaRS_widgets, atrapalha
+-- ct_ pega alguns tipo product_
+-- bp- no focalizador e projectimer nao
+
+DELETE FROM  `cursowp_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bp-|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `f5sites_67_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bp-|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `f5sites_69_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bp-|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `f5sites_75_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bp-|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `f5sites_93_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bp-|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `f5sites_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bp-|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `f5sites_pesquisa_5_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bp-|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `f5sites_pesquisa_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bp-|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `focalizador_2_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `focalizador_3_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `focalizador_4_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `focalizador_7_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `focalizador_12_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `focalizador_14_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `focalizador_15_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `focalizador_16_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `focalizador_17_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `focalizador_18_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `focalizador_19_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `focalizador_20_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `focalizador_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `franciscomat_2_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bp-|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `franciscomat_7_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bp-|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `franciscomat_9_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bp-|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `franciscomat_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bp-|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+--
+DELETE FROM  `lojasdomago_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bp-|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `pomodoros_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bp-|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `projectimer_12_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `projectimer_13_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `projectimer_14_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `projectimer_15_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `projectimer_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `sacifood_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bp-|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
+
+
+DELETE FROM  `treinamentoemfoco_options` WHERE `option_name` REGEXP '_transient|_bbp_|_cf_|_dpa_|achievements|admin_bar_disabler|atum|avhec|AWD|badgeos|bnc|boew|bp-|bulletproof|CalendarArchives_options|campaign|cf_|cp_|dp-plugin|duplicate_post|exploit|ga_|geotagphoto|google_stats|itsec|jfb|label_|mailster|mesaj|nf_|kpg|oa_social|OGC_|ossdl|postratings|qtranslate|revslider|scope_|sktnurc_|subscriptio_options|usernoise|w3tc|welcomepack|WORDPRESS_|wp_super|wpsuper|wpb|wpcumulus|wpuf|yith|ywsl';
