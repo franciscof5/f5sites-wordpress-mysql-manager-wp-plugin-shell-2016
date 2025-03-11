3f5d113cbf07b554ddf9f44ee7fd9cffc859d941
Francisco Matelli <francisco@Franciscos-MacBook-Air.local>
2025-03-11 09:57:23 -0300

    links saudations

--- mysql-saudations/end.sh
+++ mysql-saudations/end.sh
@@ -1,4 +1,3 @@
 echo "F5Sites WordPress MySQL Manager (wpsql)"
-echo "Official website: https://www.f5sites.com/software/wordpress/f5sites-wordpress-mysql-manager"
-echo "Fork Us on GitHub: https://github.com/franciscof5/f5sites-wordpress-mysql-manager"
-echo "by Francisco Matelli Matulovic | franciscomat.com | f5sites.com"
+echo "GitHub: https://github.com/franciscof5/f5sites-wordpress-mysql-manager"
+echo "by Francisco Matelli Matulovic | franciscomatelli.com.br | f5sites.com"
--- mysql-saudations/hello.sh
+++ mysql-saudations/hello.sh
@@ -1,6 +1,6 @@
-echo "F5Sites WordPress MySQL Manager (wpsql-cli)"
-echo "Official website: https://www.f5sites.com/software/wordpress/f5sites-wordpress-mysql-manager"
-echo "Fork Us on GitHub: https://github.com/franciscof5/f5sites-wordpress-mysql-manager"
+echo "F5Sites WordPress MySQL Manager (wpsql)"
+echo "GitHub: https://github.com/franciscof5/f5sites-wordpress-mysql-manager"
+echo "by Francisco Matelli Matulovic | franciscomatelli.com.br | f5sites.com"
 #echo "Don't forget to manually edit config.sh"
 echo "Hi, $USER. Current date:" $(date)
 
